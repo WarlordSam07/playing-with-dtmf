19aa0e2436d4ec0fdea06ab961f2e52fc36ef989
WarlordSam07 <pratik.lagaskar@mitaoe.ac.in>
2022-11-02 03:56:01 +0530

    Updates

---
+++ precache-manifest.0446b7ce25c8c023001c5651f36b1e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ce71311b3434f0f90dfdbbfb78125e",
+    "url": "/playing-with-dtmf/index.html"
+  },
+  {
+    "revision": "488f0c9357351067d168",
+    "url": "/playing-with-dtmf/static/css/main.9f2fbb58.chunk.css"
+  },
+  {
+    "revision": "46143222bde947d22db8",
+    "url": "/playing-with-dtmf/static/js/2.a200c2af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/playing-with-dtmf/static/js/2.a200c2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488f0c9357351067d168",
+    "url": "/playing-with-dtmf/static/js/main.f2b794e0.chunk.js"
+  },
+  {
+    "revision": "47dccb3768911eabb122",
+    "url": "/playing-with-dtmf/static/js/runtime-main.f4eaa4f3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/playing-with-dtmf/precache-manifest.0446b7ce25c8c023001c5651f36b1e71.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/playing-with-dtmf/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
